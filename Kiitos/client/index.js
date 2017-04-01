521eedbaf0792b697eaf07b83f83702076025642
Qiang Feng <Qiang.Feng@hotmail.co.uk>
2017-04-01 19:12:32 +0100

    Added create Kiitos form and added styling

--- Kiitos/client/index.js
+++ Kiitos/client/index.js
@@ -2,7 +2,7 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 Template.index.onCreated(function() {
-  this.currentPage = new ReactiveVar('home');
+  this.currentPage = new ReactiveVar('create');
 });
 
 Template.index.helpers({
@@ -16,7 +16,19 @@ Template.index.events({
     $('nav ul li').removeClass('active');
 
     let selectedLink = $(event.target).closest('li');
-    selectedLink.add('active');
+    selectedLink.addClass('active');
     template.currentPage.set(selectedLink.data('template'));
   }
 });
+
+abi = [];
+contractAddress = '0x7a1e1ded2fabcfe87af12ee7b8d7303b0e63c303';
+Contract = web3.eth.contract(abi);
+contractInstance = Contract.at(contractAddress);
+
+function createFund(name, matchPerHundredWei, continueAfterEmpty, maxMatch) {
+  // TODO: Show spinner
+  contractInstance.createFund.sendTransaction(name, matchPerHundredWei, continueAfterEmpty, {value: maxMatch}, function() {
+    // TODO: Close spinner
+  });
+}
