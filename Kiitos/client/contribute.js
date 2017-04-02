db6a2348d46047b807547d609a5df61af49db799
Qiang Feng <Qiang.Feng@hotmail.co.uk>
2017-04-02 09:08:50 +0100

    Finished cards for contribute

--- Kiitos/client/contribute.js
+++ Kiitos/client/contribute.js
@@ -7,10 +7,11 @@ Template.contribute.helpers({
   },
   weiToEth: function(wei) {
     return web3.fromWei(wei, 'ether');
+  },
+  round: function(n) {
+    return parseInt(n).toFixed(2);
   }
 });
-
-var selectedCard;
 
 Template.contribute.events({
   'click .card': function(event, template) {
@@ -29,7 +30,7 @@ Template.contribute.events({
 function contribute(id, name, message, amount) {
   $('.overlay').show();
   waiting = true;
-  contractInstance.contribute.sendTransaction(id, name, message, {value: parseInt(amount)}, function(error, data) {
+  contractInstance.contribute.sendTransaction(id, name, message, {value: web3.toWei(amount, 'ether')}, function(error, data) {
     if (error) {
       waiting = false;
       console.log('Error in contribute: ' + error);
